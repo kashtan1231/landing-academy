bbb7321e04a2e04a79a6d3255eb55f444728ce40
kashtan1231 <67584196+kashtan1231@users.noreply.github.com>
2023-09-26 18:42:58 +0300

    created BaseInput

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
     <h1>Навчання, яке працює!Відкрийте для себе найпопулярніші курси</h1>
-    <BaseInput label="Name" />
+    <BaseInput
+      v-model="name"
+      @checkField="checkField"
+      :isError="isError"
+      errorMsg="asdasd"
+      placeholder="Ваше ім’я"
+      label="Name"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import BaseInput from "./components/BaseComponents/BaseInput.vue";
+const name = ref("");
+let isError = ref(false);
+
+const checkField = (): void => {
+  console.log("hi");
+
+  isError.value = true;
+};
 </script>
 
 <style lang="scss">
--- src/components/BaseComponents/BaseInput.vue
+++ src/components/BaseComponents/BaseInput.vue
@@ -1,23 +1,76 @@
 <template>
   <div class="base-input">
-    <label for="input">{{ label }}</label>
-    <input @input="onInput" :value="modelValue" type="text" />
+    <label class="base-input__label" for="input">{{ label }}</label>
+    <input
+      :class="['base-input__input', { error: isError }]"
+      @blur="checkField"
+      @input="onInput"
+      :value="modelValue"
+      :placeholder="placeholder"
+      type="text"
+      id="input"
+    />
+    <p v-if="isError" class="base-input__error">{{ errorMsg }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ label: string; modelValue: string }>();
+const props = defineProps<{
+  label: string;
+  modelValue: string;
+  placeholder: string;
+  errorMsg: string;
+  isError: boolean;
+}>();
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+  (e: "checkField"): void;
+}>();
+
+const checkField = (): void => {
+  emit("checkField");
+};
 
 const onInput = (event: Event): void => {
-  console.log(event.target.value);
+  const target = event.target as HTMLInputElement;
+  emit("update:modelValue", target.value);
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: $font-family-default;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background-color: $gray;
+.base-input {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 488px;
+
+  &__label {
+    @extend p;
+    margin-bottom: 8px;
+    font-weight: 400;
+    line-height: 30px;
+  }
+
+  &__input {
+    @extend p;
+    padding: 16px 24px;
+    box-shadow: inset 0 0 0 1px $black;
+    border-radius: 32px;
+    background-color: $gray;
+    font-weight: 400;
+    line-height: 30px;
+    caret-color: $purple;
+  }
+
+  &__error {
+    margin-top: 8px;
+    font-weight: 400;
+    line-height: 30px;
+    color: $red;
+  }
+
+  .error {
+    box-shadow: inset 0 0 0 1px $red;
+  }
 }
 </style>
